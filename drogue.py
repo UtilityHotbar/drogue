c1efdee9f5ddcf687b5e846a2f8c7ca4a02579fd
UtilityHotbar <33590526+UtilityHotbar@users.noreply.github.com>
2022-02-10 08:58:03 +0000

    Add files via upload

--- drogue.py
+++ drogue.py
@@ -56,7 +56,7 @@ def generate_new_level(curr_depth):
 def run_fight(player):
     if check_exists(player['effects'], 'invisibility'):
         print('Invisible, you sneak by!')
-        return
+        return player
     mon_hp = roll(f'{max(round(player["level"]/5),1)}d6')+player['level']
     mon_atk = roll('1d6')+player['level']
     mon_name = ''
