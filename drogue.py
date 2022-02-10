2f663320919c236a70db07dadf40f96d28185a86
UtilityHotbar <33590526+UtilityHotbar@users.noreply.github.com>
2022-02-10 21:34:26 +0000

    Add files via upload

--- drogue.py
+++ drogue.py
@@ -369,7 +369,7 @@ def main(verbose_mode):
                     print('Commands:')
                     print('(nothing): next encounter\nr: run to next room\nh: display help\nq: exit dungeon (finalise score)\nu: use item\nc: check status')
                     print('Options (use when starting new game from command line):')
-                    print('--vb: verbose descriptions\n--seed: starting seed')
+                    print('-vb: verbose descriptions\n--seed: starting seed')
 
                 elif choice == 'r':
                     atk_chance = 0
